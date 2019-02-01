e68ec07432eb677aea05e051135f3272d5f66ce0
Gold <Gold@Gold.mshome.net>
2019-02-01 14:51:25 +0100

    mk

--- src/main/java/com/git/filter/filter.java
+++ src/main/java/com/git/filter/filter.java
@@ -74,14 +74,18 @@ public class filter implements Filter {
 //                    resp.sendError(HttpServletResponse.SC_UNAUTHORIZED);
 //                }
             }
-
-            if (reqURI.contains("/faces/index.xhtml") || (ses != null && ses.getAttribute("sessn_nums") != null) || reqURI.contains("javax.faces.resource")) {
-                chain.doFilter(request, response);
-            } else if (reqURI.contains("/faces/success/success.xhtml") || (ses == null && ses.getAttribute("sessn_nums") == null)) {
-                chain.doFilter(request, response);
-            } else {
-                resp.sendRedirect(reqt.getContextPath() + "/faces/index.xhtml");
+            else
+            {
+                 chain.doFilter(request, response);
             }
+
+//            if (reqURI.contains("/faces/index.xhtml") || (ses != null && ses.getAttribute("sessn_nums") != null) || reqURI.contains("javax.faces.resource")) {
+//               
+//            } else if (reqURI.contains("/faces/success/success.xhtml") || (ses != null && ses.getAttribute("sessn_nums") != null)) {
+//                chain.doFilter(request, response);
+//            } else {
+//                resp.sendRedirect(reqt.getContextPath() + "/faces/index.xhtml");
+//            }
 
         } catch (Exception e) {
             System.out.println(e.getMessage());
